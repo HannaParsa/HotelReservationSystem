8de746f8641ef07b082ec898353d3db473c442af
hanna parsa <hanaparsa8080@gmail.com>
2024-08-17 12:13:23 -0700

    register is in progress

--- HotelReservationSystem/HotelReservationSystem/Models/User.cs
+++ HotelReservationSystem/HotelReservationSystem/Models/User.cs
@@ -2,7 +2,7 @@
 
 namespace HotelReservationSystem.Models
 {
-    public class User: IdentityUser
+    public class User : IdentityUser
     {
         public int UserId { get; set; }
         public string Username { get; set; }
--- HotelReservationSystem/HotelReservationSystem/Program.cs
+++ HotelReservationSystem/HotelReservationSystem/Program.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HotelReservationSystem.Models;
