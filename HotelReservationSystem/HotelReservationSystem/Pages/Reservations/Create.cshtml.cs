8a104a3e652d4d8b4e60d09b7f8eac509bb61be3
hanna parsa <hanaparsa8080@gmail.com>
2024-08-17 16:21:27 -0700

    reservation is in progress

--- HotelReservationSystem/HotelReservationSystem/Pages/Reservations/Create.cshtml.cs
+++ HotelReservationSystem/HotelReservationSystem/Pages/Reservations/Create.cshtml.cs
@@ -1,84 +1,58 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HotelReservationSystem.Data;
+using HotelReservationSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using HotelReservationSystem.Models;
-using System;
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
-using HotelReservationSystem.Data;
 
 namespace HotelReservationSystem.Pages.Reservations
 {
     public class CreateModel : PageModel
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
         private readonly ApplicationDbContext _context;
 
-        public CreateModel(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext context)
+        public CreateModel(ApplicationDbContext context)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
             _context = context;
         }
 
         [BindProperty]
-        public RegisterInputModel Input { get; set; }
+        public Reservation Reservation { get; set; }
 
-        public class RegisterInputModel
+        public Room Room { get; set; }
+        public int UserId { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int roomId)
         {
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            var userId = HttpContext.Session.GetString("Username");
+            Room = await _context.Rooms.FindAsync(roomId);
 
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-
-            [Required]
-            [DataType(DataType.Password)]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-
-            
-            [Required]
-            public int RoomId { get; set; }           
+            if (Room == null)
+            {
+                return NotFound();
+            }
+            UserId = Int32.Parse(userId);
+            return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int roomId, int userId, int totalAmount)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                var user = new User
-                {
-                    Username = Input.Email,
-                    Email = Input.Email,
-                };
-                
-                var result = await _userManager.CreateAsync(user, Input.Password);
-
-                if (result.Succeeded)
-                {
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-
-                    var reservation = new Reservation
-                    {
-                        UserId = user.UserId
-                    };
-
-                    _context.Reservations.Add(reservation);
-                    await _context.SaveChangesAsync();
-
-                    return RedirectToPage("/Index");
-                }
-
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
+                return Page();
             }
 
-            return Page();
+            var reservation = new Reservation
+            {
+                UserId = userId,
+                RoomId = roomId,
+                TotalAmount = totalAmount,
+                Status = "Confirmed"
+            };
+
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Account/Index");
         }
     }
 }
