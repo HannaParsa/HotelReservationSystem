6d06260744364314922f3181aa744b97dd9591f2
hanna parsa <hanaparsa8080@gmail.com>
2024-08-20 09:32:28 -0700

    admin is in progress

---
+++ HotelReservationSystem/HotelReservationSystem/Pages/Admin/User.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelReservationSystem.Pages.Admin
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
