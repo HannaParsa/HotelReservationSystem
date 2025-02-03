c87f69c557121220bd6c664666e1e47c17f28412
hanna parsa <hanaparsa8080@gmail.com>
2025-02-03 08:14:54 -0800

    reserved dates added to detail page

--- HotelReservationSystem/HotelReservationSystem/Pages/Rooms/Details.cshtml.cs
+++ HotelReservationSystem/HotelReservationSystem/Pages/Rooms/Details.cshtml.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HotelReservationSystem.Models;
 using HotelReservationSystem.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace HotelReservationSystem.Pages.Rooms
 {
@@ -11,6 +14,7 @@ namespace HotelReservationSystem.Pages.Rooms
         private readonly ApplicationDbContext _context;
 
         public Room Room { get; set; }
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>();
 
         public DetailsModel(ApplicationDbContext context)
         {
@@ -25,6 +29,12 @@ namespace HotelReservationSystem.Pages.Rooms
             {
                 return NotFound();
             }
+
+            // Fetch all reservations for the room
+            Reservations = await _context.Reservations
+                .Where(r => r.RoomId == roomId)
+                .OrderBy(r => r.FromDate)
+                .ToListAsync();
 
             return Page();
         }
